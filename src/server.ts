d261d490e5a6a761dd38aefad9b1667aae1d24a3
Ariel Hurtado <arielhurtado388@gmail.com>
2025-10-21 09:32:14 -0500

    Agregando morgan

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import morgan from "morgan";
 import { corsConfig } from "./config/cors";
 import { conexionDB } from "./config/db";
 import proyectoRoutes from "./routes/proyectoRoutes";
@@ -13,6 +14,10 @@ const app = express();
 
 app.use(cors(corsConfig));
 
+// Logging
+app.use(morgan("dev"));
+
+// Leer datos de formularios
 app.use(express.json());
 
 // Routes
