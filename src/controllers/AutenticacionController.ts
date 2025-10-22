1fd7acdd8eaa7d863371373b748ef7b06c90fbf9
Ariel Hurtado <arielhurtado388@gmail.com>
2025-10-22 15:44:44 -0500

    Porbando git desde manjaro

--- src/controllers/AutenticacionController.ts
+++ src/controllers/AutenticacionController.ts
@@ -2,6 +2,6 @@ import { Request, Response } from "express";
 
 export class AutenticacionController {
   static crearCuenta = async (req: Request, res: Response) => {
-    res.send("Desde controller auth");
+    res.send("Desde controller auth, test");
   };
 }
