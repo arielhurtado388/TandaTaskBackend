71000ff300faec5b7fae145f81ea4ffca783cb5d
Ariel Hurtado <arielhurtado388@gmail.com>
2025-10-29 15:33:51 -0500

    RequireTLS true

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -8,6 +8,7 @@ const config = () => {
     host: process.env.SMTP_HOST,
     port: +process.env.SMTP_PORT,
     secure: false,
+    requireTLS: true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
